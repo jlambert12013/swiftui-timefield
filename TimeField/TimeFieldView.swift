44e9a1c660c995948ff9b676cd0f256a8127febf
Jim Lambert <jimlambert@Mac.lan>
2025-04-07 16:21:26 -0500

    FONTS AND PADDING ADDED

--- TimeField/TimeFieldView.swift
+++ TimeField/TimeFieldView.swift
@@ -15,113 +15,132 @@ struct TimeFieldView: View {
   
   let label: String?
   
-  enum FocusField {
-    case hour, minute, meridiem
-  }
-  
   var body: some View {
-    HStack(spacing: 4) {
+    
+    
+    VStack(alignment: .leading, spacing: 2) {
       // MARK: LABEL
       Text("\(label ?? "")").padding(.trailing)
+        .font(.subheadline)
+      //        .font(.custom("Poppins-Medium", size: 12))
       
-      // MARK: HOUR FIELD
-      TextField("HH", text: $hour)
-        .multilineTextAlignment(.trailing)
-        .keyboardType(.numberPad)
-        .autocorrectionDisabled()
-        .fixedSize()
-        .focused($focus, equals: .hour)
-        .onReceive(hour.publisher.collect()) {
-          // FILTER OUT NON-NUMERIC CHARACTERS
-          let number = $0.filter { $0.isNumber }
-          
-          // LIMIT CHARACTER COUNT
-          hour = String(number.prefix(2))
-          
-          // PREFIX WITH ZERO
-          for num in 2...9 {
-            if hour.hasPrefix("\(num)") {
-              hour = "0\(num)"
-            }
-          }
-          
-          // REMOVE SINGLE ZERO (BACKSPACE ALL)
-          if hour == "0" { hour.removeLast() }
-          
-          if hour.hasPrefix("1") {
-            // REMOVE REJECTED SUFFIX
-            for reject in 3...9 {
-              if hour.hasSuffix("\(reject)") {
-                hour.removeLast()
+      HStack {
+        // MARK: HOUR FIELD
+        TextField("HH", text: $hour)
+          .padding(.horizontal, 2)
+          .multilineTextAlignment(.trailing)
+          .keyboardType(.numberPad)
+          .autocorrectionDisabled()
+          .fixedSize()
+          .focused($focus, equals: .hour)
+          .onReceive(hour.publisher.collect()) {
+            // FILTER OUT NON-NUMERIC CHARACTERS
+            let number = $0.filter { $0.isNumber }
+            
+            // LIMIT CHARACTER COUNT
+            hour = String(number.prefix(2))
+            
+            // PREFIX WITH ZERO
+            for num in 2...9 {
+              if hour.hasPrefix("\(num)") {
+                hour = "0\(num)"
               }
             }
-          }
-          
-          if hour.count == 2 {
-            focus = .minute
-          }
-        }
-      
-      // MARK: DIVIDER
-      Text(":").opacity(0.4)
-      
-      // MARK: MINUTES
-      TextField("MM", text: $mintue)
-        .multilineTextAlignment(.leading)
-        .keyboardType(.numberPad)
-        .autocorrectionDisabled()
-        .fixedSize()
-        .focused($focus, equals: .minute)
-        .onReceive(mintue.publisher.collect()) {
-          // FILTER OUT NON-NUMERIC CHARACTERS
-          let number = $0.filter { $0.isNumber }
-          
-          // LIMIT CHARACTER COUNT
-          mintue = String(number.prefix(2))
-          
-          // REMOVE REJECTED PREFIX
-          for reject in 6...9 {
-            if mintue.hasPrefix("\(reject)") {
-              mintue.removeAll()
+            
+            // REMOVE SINGLE ZERO (BACKSPACE ALL)
+            if hour == "0" { hour.removeLast() }
+            
+            if hour.hasPrefix("1") {
+              // REMOVE REJECTED SUFFIX
+              for reject in 3...9 {
+                if hour.hasSuffix("\(reject)") {
+                  hour.removeLast()
+                }
+              }
+            }
+            
+            if hour.count == 2 {
+              focus = .minute
             }
           }
-          
-          if mintue.count == 2 {
-            focus = .meridiem
+        
+        // MARK: DIVIDER
+        Text(":").opacity(0.4)
+        
+        // MARK: MINUTES
+        TextField("MM", text: $mintue)
+          .padding(.horizontal, 2)
+          .multilineTextAlignment(.leading)
+          .keyboardType(.numberPad)
+          .autocorrectionDisabled()
+          .fixedSize()
+          .focused($focus, equals: .minute)
+          .onReceive(mintue.publisher.collect()) {
+            // FILTER OUT NON-NUMERIC CHARACTERS
+            let number = $0.filter { $0.isNumber }
+            
+            // LIMIT CHARACTER COUNT
+            mintue = String(number.prefix(2))
+            
+            // REMOVE REJECTED PREFIX
+            for reject in 6...9 {
+              if mintue.hasPrefix("\(reject)") {
+                mintue.removeAll()
+              }
+            }
+            
+            if mintue.count == 2 {
+              focus = .meridiem
+            }
+            
+          }.padding(.trailing, 2)
+        
+        // MARK: MERIDIEM
+        TextField("--", text: $meridiem)
+          .padding(.horizontal, 2)
+          .multilineTextAlignment(.leading)
+          .keyboardType(.default)
+          .autocorrectionDisabled()
+          .fixedSize()
+          .focused($focus, equals: .meridiem)
+          .onReceive(meridiem.publisher.collect()) {
+            
+            // LIMIT TO LETTER
+            let letter = $0.filter { $0.isLetter }
+            
+            // LIMIT CHARACTER COUNT
+            meridiem = String(letter.prefix(2))
+            meridiem = meridiem.uppercased()
+            meridiem = meridiem.filter { _ in
+              meridiem.contains(where: { "AP".contains($0) })
+            }
+            
+            if meridiem.count == 1 {
+              meridiem = meridiem + "M"
+            }
+            
           }
-          
-        }.padding(.trailing, 2)
-      
-      
-      // MARK: MERIDIEM
-      TextField("--", text: $meridiem)
-        .multilineTextAlignment(.leading)
-        .keyboardType(.default)
-        .autocorrectionDisabled()
-        .fixedSize()
-        .focused($focus, equals: .meridiem)
-        .onReceive(meridiem.publisher.collect()) {
-          
-          // LIMIT TO LETTER
-          let letter = $0.filter { $0.isLetter }
-          
-          // LIMIT CHARACTER COUNT
-          meridiem = String(letter.prefix(2))
-          meridiem  = meridiem.uppercased()
-          meridiem = meridiem.filter {_ in
-            meridiem.contains(where: { "AP".contains($0) })
-          }
-          
-          if meridiem.count == 1 {
-            meridiem = meridiem + "M"
-          }
-          
-         
-        }
-    }.fixedSize()
+      }
+      .padding(.vertical, 6)
+      .padding(.horizontal, 10)
+      .font(.headline)
+      .opacity(0.8)
+      .background(Color(red: 39/255, green: 39/255, blue: 42/255))
+      .foregroundStyle(Color(red: 229/255, green: 231/255, blue: 235/255))
+      .cornerRadius(6)
+      .fixedSize()
+    }
   }
 }
 
+extension TimeFieldView {
+  enum FocusField {
+    case hour, minute, meridiem
+  }
+}
+
+
+// PREVIEW
 struct TimeFieldView_Previews: PreviewProvider {
   static var previews: some View {
     TimeFieldView(label: "Enter Time")
@@ -129,7 +148,4 @@ struct TimeFieldView_Previews: PreviewProvider {
 }
 
 
-enum Meridiem: String {
-  case am = "AM"
-  case pm = "PM"
-}
+//rgb(229, 231, 235);
